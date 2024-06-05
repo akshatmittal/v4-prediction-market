3c5115fabf49b9a0adaeacb5cd7e54d6ae64a55a
Akshat Mittal <itsreallyakshat@gmail.com>
2024-06-06 04:51:34 +0530

    cleanup

--- src/prediction-market/Fixed192x64Math.sol
+++ src/prediction-market/Fixed192x64Math.sol
@@ -15,9 +15,9 @@ library Fixed192x64Math {
      *  Constants
      */
     // This is equal to 1 in our calculations
-    uint public constant ONE = 0x10000000000000000;
-    uint public constant LN2 = 0xb17217f7d1cf79ac;
-    uint public constant LOG2_E = 0x171547652b82fe177;
+    uint256 public constant ONE = 0x10000000000000000;
+    uint256 public constant LN2 = 0xb17217f7d1cf79ac;
+    uint256 public constant LOG2_E = 0x171547652b82fe177;
 
     /*
      *  Public functions
@@ -25,7 +25,7 @@ library Fixed192x64Math {
     /// @dev Returns natural exponential function value of given x
     /// @param x x
     /// @return e**x
-    function exp(int x) public pure returns (uint) {
+    function exp(int256 x) public pure returns (uint256) {
         // revert if x is > MAX_POWER, where
         // MAX_POWER = int(mp.floor(mp.log(mpf(2**256 - 1) / ONE) * ONE))
         require(x <= 2454971259878909886679);
@@ -34,7 +34,7 @@ library Fixed192x64Math {
         if (x <= -818323753292969962227) return 0;
 
         // Transform so that e^x -> 2^x
-        (uint lower, uint upper) = pow2Bounds((x * int(ONE)) / int(LN2));
+        (uint256 lower, uint256 upper) = pow2Bounds((x * int256(ONE)) / int256(LN2));
         return (upper - lower) / 2 + lower;
     }
 
@@ -42,11 +42,8 @@ library Fixed192x64Math {
     /// @param x exponent in fixed point
     /// @param estimationMode whether to return a lower bound, upper bound, or a midpoint
     /// @return estimate of 2**x in fixed point
-    function pow2(
-        int x,
-        EstimationMode estimationMode
-    ) public pure returns (uint) {
-        (uint lower, uint upper) = pow2Bounds(x);
+    function pow2(int256 x, EstimationMode estimationMode) public pure returns (uint256) {
+        (uint256 lower, uint256 upper) = pow2Bounds(x);
         if (estimationMode == EstimationMode.LowerBound) {
             return lower;
         }
@@ -65,7 +62,7 @@ library Fixed192x64Math {
     ///   "lower": "lower bound of 2**x in fixed point",
     ///   "upper": "upper bound of 2**x in fixed point"
     /// }
-    function pow2Bounds(int x) public pure returns (uint lower, uint upper) {
+    function pow2Bounds(int256 x) public pure returns (uint256 lower, uint256 upper) {
         // revert if x is > MAX_POWER, where
         // MAX_POWER = int(mp.floor(mp.log(mpf(2**256 - 1) / ONE, 2) * ONE))
         require(x <= 3541774862152233910271);
@@ -76,14 +73,14 @@ library Fixed192x64Math {
         // 2^x = 2^(floor(x)) * 2^(x-floor(x))
         //       ^^^^^^^^^^^^^^ is a bit shift of ceil(x)
         // so Taylor expand on z = x-floor(x), z in [0, 1)
-        int shift;
-        int z;
+        int256 shift;
+        int256 z;
         if (x >= 0) {
-            shift = x / int(ONE);
-            z = x % int(ONE);
+            shift = x / int256(ONE);
+            z = x % int256(ONE);
         } else {
-            shift = (x + 1) / int(ONE) - 1;
-            z = x - (int(ONE) * shift);
+            shift = (x + 1) / int256(ONE) - 1;
+            z = x - (int256(ONE) * shift);
         }
         assert(z >= 0);
         // 2^x = 1 + (ln 2) x + (ln 2)^2/2! x^2 + ...
@@ -94,84 +91,83 @@ library Fixed192x64Math {
         // >>> coeffs = [mp.log(2)**i / mp.factorial(i) for i in range(1, 21)]
         // >>> shifts = [64 - int(mp.log(c, 2)) for c in coeffs]
         // >>> print('\n'.join(hex(int(c * (1 << s))) + ', ' + str(s) for c, s in zip(coeffs, shifts)))
-        int result = int(ONE) << 64;
-        int zpow = z;
+        int256 result = int256(ONE) << 64;
+        int256 zpow = z;
         result += 0xb17217f7d1cf79ab * zpow;
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xf5fdeffc162c7543 * zpow) >> (66 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xe35846b82505fc59 * zpow) >> (68 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0x9d955b7dd273b94e * zpow) >> (70 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xaec3ff3c53398883 * zpow) >> (73 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xa184897c363c3b7a * zpow) >> (76 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xffe5fe2c45863435 * zpow) >> (80 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xb160111d2e411fec * zpow) >> (83 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xda929e9caf3e1ed2 * zpow) >> (87 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xf267a8ac5c764fb7 * zpow) >> (91 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xf465639a8dd92607 * zpow) >> (95 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xe1deb287e14c2f15 * zpow) >> (99 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xc0b0c98b3687cb14 * zpow) >> (103 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0x98a4b26ac3c54b9f * zpow) >> (107 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xe1b7421d82010f33 * zpow) >> (112 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0x9c744d73cfc59c91 * zpow) >> (116 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         result += (0xcc2225a0e12d3eab * zpow) >> (121 - 64);
-        zpow = (zpow * z) / int(ONE);
+        zpow = (zpow * z) / int256(ONE);
         zpow = (0xfb8bb5eda1b4aeb9 * zpow) >> (126 - 64);
         result += zpow;
-        zpow = int(8 * ONE);
+        zpow = int256(8 * ONE);
 
         shift -= 64;
         if (shift >= 0) {
-            uint ushift = uint(shift);
+            uint256 ushift = uint256(shift);
             if (result >> (256 - ushift) == 0) {
-                lower = uint(result) << ushift;
+                lower = uint256(result) << ushift;
                 zpow <<= ushift; // todo: is this safe?
-                if (lower + uint(zpow) >= lower) upper = lower + uint(zpow);
+                if (lower + uint256(zpow) >= lower) upper = lower + uint256(zpow);
                 else upper = 2 ** 256 - 1;
                 return (lower, upper);
-            } else return (2 ** 256 - 1, 2 ** 256 - 1);
+            } else {
+                return (2 ** 256 - 1, 2 ** 256 - 1);
+            }
         }
         // We'll only get here if shfit is negative
-        uint nshift = uint(-shift);
+        uint256 nshift = uint256(-shift);
         // zpow = (zpow >> (-shift)) + 1;
         // lower = uint(result) >> (-shift);
         zpow = (zpow >> (nshift)) + 1;
-        lower = uint(result) >> (nshift);
-        upper = lower + uint(zpow);
+        lower = uint256(result) >> (nshift);
+        upper = lower + uint256(zpow);
         return (lower, upper);
     }
 
     /// @dev Returns natural logarithm value of given x
     /// @param x x
     /// @return ln(x)
-    function ln(uint x) public pure returns (int) {
-        (int lower, int upper) = log2Bounds(x);
-        return (((upper - lower) / 2 + lower) * int(ONE)) / int(LOG2_E);
+    function ln(uint256 x) public pure returns (int256) {
+        (int256 lower, int256 upper) = log2Bounds(x);
+        return (((upper - lower) / 2 + lower) * int256(ONE)) / int256(LOG2_E);
     }
 
     /// @dev Returns estimate of binaryLog(x) given x
     /// @param x logarithm argument in fixed point
     /// @param estimationMode whether to return a lower bound, upper bound, or a midpoint
     /// @return estimate of binaryLog(x) in fixed point
-    function binaryLog(
-        uint x,
-        EstimationMode estimationMode
-    ) public pure returns (int) {
-        (int lower, int upper) = log2Bounds(x);
+    function binaryLog(uint256 x, EstimationMode estimationMode) public pure returns (int256) {
+        (int256 lower, int256 upper) = log2Bounds(x);
         if (estimationMode == EstimationMode.LowerBound) {
             return lower;
         }
@@ -190,28 +186,28 @@ library Fixed192x64Math {
     ///   "lower": "lower bound of binaryLog(x) in fixed point",
     ///   "upper": "upper bound of binaryLog(x) in fixed point"
     /// }
-    function log2Bounds(uint x) public pure returns (int lower, int upper) {
+    function log2Bounds(uint256 x) public pure returns (int256 lower, int256 upper) {
         require(x > 0);
         // compute ⌊log₂x⌋
         lower = floorLog2(x);
 
-        uint y;
-        if (lower < 0) y = x << uint(-lower);
-        else y = x >> uint(lower);
+        uint256 y;
+        if (lower < 0) y = x << uint256(-lower);
+        else y = x >> uint256(lower);
 
-        lower *= int(ONE);
+        lower *= int256(ONE);
 
         // y = x * 2^(-⌊log₂x⌋)
         // so 1 <= y < 2
         // and log₂x = ⌊log₂x⌋ + log₂y
-        for (int m = 1; m <= 64; m++) {
+        for (int256 m = 1; m <= 64; m++) {
             if (y == ONE) {
                 break;
             }
             y = (y * y) / ONE;
             if (y >= 2 * ONE) {
-                uint um = uint(m);
-                lower += int(ONE >> um);
+                uint256 um = uint256(m);
+                lower += int256(ONE >> um);
                 y /= 2;
             }
         }
@@ -222,16 +218,13 @@ library Fixed192x64Math {
     /// @dev Returns base 2 logarithm value of given x
     /// @param x x
     /// @return lo logarithmic value
-    function floorLog2(uint x) public pure returns (int lo) {
+    function floorLog2(uint256 x) public pure returns (int256 lo) {
         lo = -64;
-        int hi = 193;
+        int256 hi = 193;
         // I use a shift here instead of / 2 because it floors instead of rounding towards 0
-        int mid = (hi + lo) >> 1;
+        int256 mid = (hi + lo) >> 1;
         while ((lo + 1) < hi) {
-            if (
-                (mid < 0 && x << uint(-mid) < ONE) ||
-                (mid >= 0 && x >> uint(mid) < ONE)
-            ) hi = mid;
+            if ((mid < 0 && x << uint256(-mid) < ONE) || (mid >= 0 && x >> uint256(mid) < ONE)) hi = mid;
             else lo = mid;
             mid = (hi + lo) >> 1;
         }
@@ -240,10 +233,11 @@ library Fixed192x64Math {
     /// @dev Returns maximum of an array
     /// @param nums Numbers to look through
     /// @return maxNum Maximum number
-    function max(int[] memory nums) public pure returns (int maxNum) {
+    function max(int256[] memory nums) public pure returns (int256 maxNum) {
         require(nums.length > 0);
         maxNum = -2 ** 255;
-        for (uint i = 0; i < nums.length; i++)
+        for (uint256 i = 0; i < nums.length; i++) {
             if (nums[i] > maxNum) maxNum = nums[i];
+        }
     }
 }
--- test/Market.t.sol
+++ test/Market.t.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "../src/gnosispm/Market.sol";
-import "../src/gnosispm/MarketMaker.sol";
-import "../src/gnosispm/Event.sol";
-import "../src/gnosispm/MockERC20.sol"; // MockERC20 is a mock contract for ERC20
+import "../src/prediction-market/Market.sol";
+import "../src/prediction-market/MarketMaker.sol";
+import "../src/prediction-market/Event.sol";
+import "../src/prediction-market/MockERC20.sol"; // MockERC20 is a mock contract for ERC20
 
 contract MarketTest is Test {
     Market public market;
@@ -17,7 +17,7 @@ contract MarketTest is Test {
     address public creator = address(1);
     address public buyer = address(2);
     address public seller = address(3);
-    uint public fundingAmount = 1000;
+    uint256 public fundingAmount = 1000;
 
     function setUp() public {
         // Deploy the mock ERC20 token
@@ -26,22 +26,9 @@ contract MarketTest is Test {
         collateralToken.mint(buyer, 10 ** 18);
         collateralToken.mint(seller, 10 ** 18);
 
-        // Deploy the mock Event contract
-        // eventContract = new MockEvent(address(collateralToken));
         eventContract = new CategoricalEvent(collateralToken, 2);
-
         marketMaker = new MarketMaker();
-
-        // Deploy the Market contract
         market = new Market(creator, eventContract, marketMaker, 10000);
-
-        // Initialize the Market contract
-        // market.initialize(address(eventContract), creator);
-
-        // Set initial state for the Market contract
-        // market.setCreator(creator);
-        // market.setStage(Market.Stages.MarketCreated);
-        // market.setFee(10000); // 1% fee
     }
 
     function testFundMarket() public {
@@ -52,7 +39,7 @@ contract MarketTest is Test {
         vm.stopPrank();
 
         assertEq(market.funding(), fundingAmount);
-        assertEq(uint(market.stage()), uint(MarketData.Stages.MarketFunded));
+        assertEq(uint256(market.stage()), uint256(MarketData.Stages.MarketFunded));
     }
 
     function testCloseMarket() public {
@@ -62,14 +49,14 @@ contract MarketTest is Test {
         market.close();
         vm.stopPrank();
 
-        assertEq(uint(market.stage()), uint(MarketData.Stages.MarketClosed));
+        assertEq(uint256(market.stage()), uint256(MarketData.Stages.MarketClosed));
     }
 
     function testWithdrawFees() public {
         vm.startPrank(creator);
         collateralToken.approve(address(market), fundingAmount);
         market.fund(fundingAmount);
-        uint fees = market.withdrawFees();
+        uint256 fees = market.withdrawFees();
         vm.stopPrank();
 
         // No fees?
@@ -84,7 +71,7 @@ contract MarketTest is Test {
 
         vm.startPrank(buyer);
         collateralToken.approve(address(market), fundingAmount);
-        uint cost = market.buy(0, 10, 100);
+        uint256 cost = market.buy(0, 10, 100);
         vm.stopPrank();
 
         // TODO - is this right?
@@ -99,12 +86,12 @@ contract MarketTest is Test {
 
         vm.startPrank(buyer);
         collateralToken.approve(address(market), fundingAmount * 2);
-        uint cost = market.buy(0, 10, 100);
+        uint256 cost = market.buy(0, 10, 100);
 
         // FAILS HERE!
         // tradeImpl tries to transfer 115792089237316195423570985008687907853269984665640564039457584007913129639934
         collateralToken.approve(address(market), fundingAmount);
-        uint profit = market.sell(0, 5, 1);
+        uint256 profit = market.sell(0, 5, 1);
         vm.stopPrank();
         assertGt(profit, 0);
     }
@@ -118,7 +105,7 @@ contract MarketTest is Test {
         vm.startPrank(seller);
         // tradeImpl tries to transfer 115792089237316195423570985008687907853269984665640564039457584007913129639934
         collateralToken.approve(address(market), fundingAmount);
-        uint profit = market.shortSell(0, 10, 1);
+        uint256 profit = market.shortSell(0, 10, 1);
         vm.stopPrank();
         assertGt(profit, 0);
     }
